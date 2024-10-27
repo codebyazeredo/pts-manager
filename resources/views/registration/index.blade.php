a3b6a31a0a133d560768d15ed3bdf83c4688941f
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-27 16:03:43 -0300

    Feat: Adicionado modal para cadastrar clientes

--- resources/views/registration/index.blade.php
+++ resources/views/registration/index.blade.php
@@ -14,16 +14,22 @@
         </div>
     </x-slot>
 
+    @include('registration.modal-create')
+
             {{--Sessão Principal--}}
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
-                    {{ __("SESSÃO DE CADASTROS") }}
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-create-client">
+                        Cadastrar novo cliente
+                    </button>
 
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+
