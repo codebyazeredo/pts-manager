16ca4b9a8c75795e56c2e6f98afebbb600ed92f9
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-28 21:07:10 -0300

    Feat: Adicionado People controller

---
+++ app/Http/Controllers/PeopleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\People;
+use Illuminate\Http\Request;
+
+class PeopleController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function show(People $people)
+    {
+
+    }
+
+    public function edit(People $people)
+    {
+
+    }
+
+    public function update(Request $request, People $people)
+    {
+
+    }
+
+    public function destroy(People $people)
+    {
+
+    }
+}
