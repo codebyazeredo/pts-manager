acd79b0e3ff1c694078ce4462d4a8566b75591f4
codebyazeredo <matheus.azeredo@hotmail.com>
2024-10-31 19:47:04 -0300

    Fix: Mudança completa no corpo do projeto

--- resources/views/animal/submenu.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="flex space-x-8">
-    <x-nav-link href="#" active="">
-        {{ __('Cadastros de Animais') }}
-    </x-nav-link>
-    <x-nav-link href="#" active="">
-        {{ __('Históricos de Animais') }}
-    </x-nav-link>
-</div>
--- resources/views/registration/submenu.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="flex space-x-8">
-    <x-nav-link href="#" active="">
-        {{ __('Cadastros de Clientes') }}
-    </x-nav-link>
-    <x-nav-link href="#" active="">
-        {{ __('Históricos de Clientes') }}
-    </x-nav-link>
-</div>
